7d27175e37a4eca594b70b2e2a2f04cb87fe2c81
Abolfazl Sabagh <a.sabagh72@gmail.com>
2019-08-14 19:08:31 +0430

    javascript let , const

---
+++ let-const.js
@@ -0,0 +1,9 @@
+var x = 10;
+{
+    let x = 5;
+    var y = 12;
+    const x2 = "constant value is like let localization";
+    console.log("let x = " + x);
+}
+console.log("var x = " + x);
+console.log("var y = " + y);
