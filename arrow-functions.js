414084934db53f3beb60b16559dd89a710fa17e3
Abolfazl Sabagh <a.sabagh72@gmail.com>
2019-08-14 19:10:04 +0430

    JavaScript Arrow Functions

---
+++ arrow-functions.js
@@ -0,0 +1,17 @@
+/*var x = function(x,y){
+    console.log(x*y);
+}
+*/
+
+/*var x = (x,y) => console.log(x*y);*/
+
+
+const x = (x,y) => {
+    let tempX= x;
+    let tempY = y;
+    console.log(tempX*tempY);
+}
+
+// console.log(tempX);
+x(3,4);
+/* x(2,3); */
